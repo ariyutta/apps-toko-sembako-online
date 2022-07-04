ed9c3f8692ec851872a86fef46951f5464b61481
saradauchiha93 <saradauchiha93@gmail.com>
2022-07-04 22:13:28 +0700

    fix tampilan status Pesanan 1

--- app/Http/Controllers/DataPesananController.php
+++ app/Http/Controllers/DataPesananController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DataPesanan;
 use Illuminate\Http\Request;
 
 class DataPesananController extends Controller
 {
     public function data_pesanan() {
         $title_admin = 'Data Pesanan';
+        $data_pesanan = DataPesanan::all();
 
-        return view('admins.data_pesanan.index', compact('title_admin'));
+        return view('admins.data_pesanan.index', compact('title_admin','data_pesanan'));
     }
 }
